64d32f8ccedb5b0ee1abdca1d266aac65588d943
Priscila <57264298+PriscilaMarin@users.noreply.github.com>
2019-11-01 19:26:59 +0200

    Update T1_E3_Marin_Priscila_Dana_424D.m

--- T1_E3_Marin_Priscila_Dana_424D.m
+++ T1_E3_Marin_Priscila_Dana_424D.m
@@ -1,5 +1,5 @@
 %%%%E3:
-x=rand(1,2)+rand(1,2)*i %%Generarea aleatoare a unui nr complex
+x=rand(1,2)+rand(1,2)*i %%Generarea aleatoare a unui vector cu 2 numere complexe
 [Media_aritm,Vpatrat, Matricex]=fctcomplex(x);%%Apelarea functiei fctcomplex cu parametrul efectiv vectorul x
 Media_aritm
 Vpatrat
