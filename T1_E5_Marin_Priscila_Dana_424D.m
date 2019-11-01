6369764d7ede9cefe9c5fccb4d6dcf5892389baf
Priscila <57264298+PriscilaMarin@users.noreply.github.com>
2019-11-01 19:46:42 +0200

    Update T1_E5_Marin_Priscila_Dana_424D.m

--- T1_E5_Marin_Priscila_Dana_424D.m
+++ T1_E5_Marin_Priscila_Dana_424D.m
@@ -1,14 +1,20 @@
 %%%E5:
+%a:
 F = 50;
 t = 0:0.01:0.2;
 s = 2*sin(2*pi*F*t);
 figure
 plot(t,s,'.-'),xlabel('Timp [s]'),grid %%NU sunt destule puncte pentru a aproxima corect un sinus
+%b:
+x=[t(1),0];  %%Proiectia pe ox a primului punct de pe grafic
+y=[t(length(t)),0];  %%Proiectia pe ox a ultimului punct de pe grafic
+D = pdist([x',y'],'cityblock')  %%Calculul distantei pe ox, adica a perioadei
+%c:
 figure
 t = 0:0.0002:0.2;
 s = 2*sin(2*pi*F*t); 
 plot(t,s,'.-'),xlabel('Timp [s]'),grid    
-hold on;
+hold on; %%Desenarea a doua grafice ce acelasi sistem de coordonate
 F=20;
 s=2*cos(2*pi*F*t);
 plot(t,s,'-'),xlabel('Timp [s]'),grid 
