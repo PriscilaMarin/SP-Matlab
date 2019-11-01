0fb703670717eff428b32b69ee713da0ffee4c42
Priscila <57264298+PriscilaMarin@users.noreply.github.com>
2019-11-01 19:01:54 +0200

    Update T1_ex5_Marin_Priscila_Dana_424D.m

--- T1_ex5_Marin_Priscila_Dana_424D.m
+++ T1_ex5_Marin_Priscila_Dana_424D.m
@@ -2,45 +2,26 @@
 %%Rezolutia temporara Ts=2ms
 T=3;
 Ts=2e-3;
-t=0:Ts:3*T; %%Vom afisa 3 perioade 
-for (k=1:length(t))  %%Parcurgem vectorul t cu indexul k
-    x=1.5*sin(2*pi*1/T*t(k)); %%Sinus de amplitudine 1.5 la momentul de timp t(k)
-    if (x>0)
-        y(k)= 1.5*sin(2*pi*1/T*t(k));
-    else
-        y(k)=-1.5*sin(2*pi*1/T*t(k)); %%Modulam dublu alternanta semnalul sinusoidal
-    end
-end
+t=0:Ts:2*T; %%Vom afisa 2 perioade 
+y= abs(1.5*sin(2*pi*1/T*t)); %%Redresam dublu alternanta semnalul prin modul
 figure
 plot(t,y), title('Sinus dublualternat cu rezolutia temporala 2ms');
 clear all;
+
 %%Rezolutia temporara Ts=20ms
 T=3;
 Ts=2e-2;
-t=0:Ts:3*T; %%Vom afisa 3 perioade 
-for (k=1:length(t))  %%Parcurgem vectorul t cu indexul k
-    x=1.5*sin(2*pi*1/T*t(k)); %%Sinus de amplitudine 1.5 la momentul de timp t(k)
-    if (x>0)
-        y(k)= 1.5*sin(2*pi*1/T*t(k));
-    else
-        y(k)=-1.5*sin(2*pi*1/T*t(k)); %%Modulam dublu alternanta semnalul sinusoidal
-    end
-end
+t=0:Ts:2*T; %%Vom afisa 3 perioade 
+y= abs(1.5*sin(2*pi*1/T*t)); %%Redresam dublu alternanta semnalul prin modul
 figure
 plot(t,y), title('Sinus dublualternat cu rezolutia temporala 20ms');
 clear all;
+
 %%Rezolutia temporara Ts=200ms
 T=3;
 Ts=2e-1;
-t=0:Ts:3*T; %%Vom afisa 3 perioade 
-for (k=1:length(t))  %%Parcurgem vectorul t cu indexul k
-    x=1.5*sin(2*pi*1/T*t(k)); %%Sinus de amplitudine 1.5 la momentul de timp t(k)
-    if (x>0)
-        y(k)= 1.5*sin(2*pi*1/T*t(k));
-    else
-        y(k)=-1.5*sin(2*pi*1/T*t(k)); %%Modulam dublu alternanta semnalul sinusoidal
-    end
-end
+t=0:Ts:2*T; %%Vom afisa 3 perioade 
+y= abs(1.5*sin(2*pi*1/T*t)); %%Redresam dublu alternanta semnalul prin modul
 figure
 plot(t,y), title('Sinus dublualternat cu rezolutia temporala 200ms');
 clear all;
