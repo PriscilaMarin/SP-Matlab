bb307edb452642eaab4dd624725a2f19cf419398
Priscila <57264298+PriscilaMarin@users.noreply.github.com>
2019-11-01 19:54:21 +0200

    Update T1_E6_Marin_Priscila_Dana_424D.m

--- T1_E6_Marin_Priscila_Dana_424D.m
+++ T1_E6_Marin_Priscila_Dana_424D.m
@@ -1,17 +1,17 @@
 %%%E6:
-N= 5;%%Numarul de biti
-T=N*0.5; %%Calculul perioadei semnalului in functie de nr de biti
+N= 5;%%Numarul de trepte de 0 sau 1 dintr-o perioada
+T=N*0.5; %%Calculul perioadei semnalului in functie de nr de trepte
 Ts=1/12000;  %%Perioada de esantionare(1/frecventa esantionare)
 M=0.5*12000; %%Puncte plotate pentru fiecare bit
+semnal=[];
 for(k=1:N)  %% pentru fiecare bit
-    Amplitudine=randi(2)-1;  %% Amplitudinea bitului k este aleasa aleator, fie 0 fie 1
-   for(p=1:M)  %%pentru fiecare punct al bitului k
-       Vector(p+(k-1)*M)=Amplitudine; 
-   end
+    Amplitudine=randi(2)-1;  %% Amplitudinea treptei k este aleasa aleator, fie 0 fie 1
+    bit=Amplitudine*ones(1,M); %%Construim treapta de amplitudine 0 sau 1;
+    semnal=[semnal,bit]; %%Adaugam o treapta la semnal
 end
 t=0:Ts:T-Ts;
 figure
 subplot(1,2,1)
-plot(t,Vector,'-'),xlabel('Timp [ms]'),grid
+plot(t,semnal,'-'),xlabel('Timp [ms]'),grid  %%Desenam grafic semnalul
 subplot(1,2,2)
-stem(t,Vector,'-'),xlabel('Timp [ms]'),grid
+stem(t,semnal,'-'),xlabel('Timp [ms]'),grid
